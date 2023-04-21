73dff5861fd513438182668f7a5a3970db50fafd
Алексей Гришин <75268007+shlexsoid@users.noreply.github.com>
2023-04-21 23:41:56 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,11 +1,10 @@
 import pandas as pd
 import numpy as np
+from scipy.stats import anderson_ksamp
 
-
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 399623382 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array, y: np.array) -> bool:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    res = anderson_ksamp([x,y])
+    return res.pvalue < 0.07
+
